984089de391ce11cf3745c7231eb053b46ae2bdc
iamjsk10 <98348336+iamjsk10@users.noreply.github.com>
2024-09-20 23:28:48 -0400

    updated project

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,20 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl, link }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      
+      <div className="proj-imgbx">
+        <img src={imgUrl} className="project-image" />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+          <br /> {/* Line break added here */}
+          <a href={link} target="_blank" rel="noopener noreferrer" className="view-code">
+            Know More
+          </a>
+        </div>
+      </div>
+    </Col>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,120 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/yt-spoti.jpeg";
+import projImg2 from "../assets/img/ml.jpg";
+import projImg3 from "../assets/img/movies.jpeg";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import gameBanner from "../assets/img/game.png"
+import rpgBanner from "../assets/img/rpg.jpeg"
+
+
+export const Projects = () => {
+
+  const personalProjects = [
+    {
+      title: "YT music to Spotify",
+      description: "Python program which converts youtube playlist to spotify using YT & Spotify's API",
+      imgUrl: projImg1,
+      link: "https://github.com/iamjsk10/YT_Music_to_Spotify/wiki",
+      category: 'personal'
+    },
+    {
+      title: "Customer Drawing Classifier",
+      description: "GUI implemted program which let's the user to train the model by making custom drawings.",
+      imgUrl: projImg2,
+      link: "https://github.com/iamjsk10/AI_Custom_Drawing_Classifier/wiki",
+      category: 'personal'
+    },
+    {
+      title: "Movie Recommendation",
+      description: "Made on Google collabs using Python, NumPy and Pandas",
+      imgUrl: projImg3,
+      link: "https://colab.research.google.com/drive/1OAWZbjjXbGPwYJuWfmaRMz_cOhB8aU01?usp=sharing",
+      category: 'personal'
+    }
+  ];
+  
+  const academicProjects = [
+    {
+      title: 'RPG Game',
+      description: 'Top down RPG game programmed with C++',
+      imgUrl: rpgBanner,
+      link: 'https://github.com/iamjsk10/Comp345_FinalProject/wiki',
+    },
+    
+  ];
+
+  const gameJamProjects = [
+    {
+      title: ' Midnight Metro',
+      description: 'First Person shooter made with Unity',
+      imgUrl: gameBanner,
+      link: 'https://oksoft.itch.io/midnight-metro',
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+              <div className="animate__animated animate__fadeIn">
+                <h2>Projects</h2>
+                <p></p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Personal</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Academic</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">GameJam</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className="animate__animated animate__slideInUp">
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {personalProjects.map
+                          ((project, index) => 
+                            (
+                            <ProjectCard key={index} {...project} />
+                            )
+                          )
+                      }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                      {academicProjects.map
+                        ((project, index) => 
+                          (
+                            <ProjectCard key={index} {...project} />
+                          )
+                        )
+                      }
+
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <Row>
+                    {gameJamProjects.map
+                        ((project, index) => 
+                          (
+                            <ProjectCard key={index} {...project} />
+                          )
+                        )
+                      }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
